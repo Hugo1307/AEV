856d7acd774b06f2623d296e440751c4dd9affdd
Sevelantis <hamer223@o2.pl>
2022-12-07 03:04:36 +0000

    Fixed resource busy issue.

--- CTF/Backend/cron/cron_manager.py
+++ CTF/Backend/cron/cron_manager.py
@@ -7,8 +7,20 @@ class CronManager:
         self.cron_file = '/var/spool/cron/crontabs/root'
         self.time_regex = '([0-9].*\ |\*\ ){5}'
         self.cleaner_command = f'sh /app/disk_cleaner.sh \| xargs >> /var/log/cron.log'
+        self.restart_cron_service()
+        
     
     def update_cleaner_time(self, new_time=None):
-        sed_command = f'sed -i -E "s:^{self.time_regex}(\ *){self.cleaner_command}$:{new_time} {self.cleaner_command}:" {self.cron_file}'
-        return os.popen(sed_command).read()
+        cp_command  = f'cp {self.cron_file} tmp.txt'
+        sed_command = f'sed -i -E "s:^{self.time_regex}.*{self.cleaner_command}$:{new_time} {self.cleaner_command}:" tmp.txt'
+        cat_command = f'cat tmp.txt > {self.cron_file}'  
+        os.popen(cp_command)
+        os.popen(sed_command)
+        os.popen(cat_command)
+        self.restart_cron_service()
+        return sed_command
+    
+    def restart_cron_service(self):
+        restart_command = 'crond restart'
+        os.popen(restart_command)
     
